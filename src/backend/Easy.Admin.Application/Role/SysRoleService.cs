e1dd054356a2ec1ac0552b71ee5ba187f6c66220
念舊 <823302316@qq.com>
2023-05-12 18:12:14 +0800

    新增角色接口

---
+++ src/backend/Easy.Admin.Application/Role/SysRoleService.cs
@@ -0,0 +1,103 @@
+﻿using Easy.Admin.Application.Role.Dtos;
+
+namespace Easy.Admin.Application.Role;
+
+/// <summary>
+/// 角色管理
+/// </summary>
+public class SysRoleService : BaseService<SysRole>
+{
+    private readonly ISqlSugarRepository<SysRole> _sysRoleRepository;
+    private readonly ISqlSugarRepository<SysRoleMenu> _sysRoleMenuRepository;
+    private readonly IIdGenerator _idGenerator;
+
+    public SysRoleService(ISqlSugarRepository<SysRole> sysRoleRepository,
+        ISqlSugarRepository<SysRoleMenu> sysRoleMenuRepository,
+        IIdGenerator idGenerator) : base(sysRoleRepository)
+    {
+        _sysRoleRepository = sysRoleRepository;
+        _sysRoleMenuRepository = sysRoleMenuRepository;
+        _idGenerator = idGenerator;
+    }
+
+    /// <summary>
+    /// 角色分页查询
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<PageResult<SysRolePageInput>> Page([FromQuery] SysRoleQueryInput dto)
+    {
+        return await _sysRoleRepository.AsQueryable()
+            .WhereIF(!string.IsNullOrWhiteSpace(dto.Name), x => x.Name.Contains(dto.Name))
+            .OrderBy(x => x.Sort)
+            .OrderByDescending(x => x.Id)
+            .Select(x => new SysRolePageInput
+            {
+                Id = x.Id,
+                Name = x.Name,
+                CreatedTime = x.CreatedTime,
+                Status = x.Status,
+                Code = x.Code,
+                Sort = x.Sort,
+                Remark = x.Remark
+            }).ToPagedListAsync(dto);
+    }
+
+    /// <summary>
+    /// 添加角色
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [Description("添加角色")]
+    [HttpPost("add")]
+    [UnitOfWork]
+    public async Task AddRole(AddSysRoleInput dto)
+    {
+        if (await _sysRoleRepository.IsAnyAsync(x => x.Code == dto.Code))
+        {
+            throw Oops.Bah("角色编码已存在");
+        }
+
+        var role = dto.Adapt<SysRole>();
+        role.Id = _idGenerator.NewLong();
+        var roleMenus = dto.Menus.Select(x => new SysRoleMenu()
+        {
+            MenuId = x,
+            RoleId = role.Id
+        }).ToList();
+        await _sysRoleRepository.InsertAsync(role);
+        await _sysRoleMenuRepository.InsertRangeAsync(roleMenus);
+    }
+
+    /// <summary>
+    /// 更新角色
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [Description("更新角色")]
+    [HttpPut("edit")]
+    [UnitOfWork]
+    public async Task UpdateRole(UpdateSysRoleInput dto)
+    {
+        var sysRole = await _sysRoleRepository.GetByIdAsync(dto.Id);
+        if (sysRole == null)
+        {
+            throw Oops.Bah("无效参数");
+        }
+        if (await _sysRoleRepository.IsAnyAsync(x => x.Id != dto.Id && x.Code == dto.Code))
+        {
+            throw Oops.Bah("角色编码已存在");
+        }
+
+        dto.Adapt(sysRole);
+        var roleMenus = dto.Menus.Select(x => new SysRoleMenu()
+        {
+            MenuId = x,
+            RoleId = sysRole.Id
+        }).ToList();
+        await _sysRoleRepository.UpdateAsync(sysRole);
+        await _sysRoleMenuRepository.DeleteAsync(x => x.RoleId == sysRole.Id);
+        await _sysRoleMenuRepository.InsertRangeAsync(roleMenus);
+    }
+}
