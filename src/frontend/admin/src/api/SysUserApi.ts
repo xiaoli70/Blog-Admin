75db711e332d21c4da3dcf78a39fb82a3b028846
念舊 <823302316@qq.com>
2023-05-22 16:47:22 +0800

    no commit message

---
+++ src/frontend/admin/src/api/SysUserApi.ts
@@ -0,0 +1,37 @@
+import { http } from '../utils/request';
+import { PageResultSysUserPageOutput, AddSysUserInput, UpdateSysUserInput } from './models';
+
+/**
+ * 系统用户分页查询
+ * @param params 查询参数
+ * @returns
+ */
+export const getSysUserPage = (params: any) => {
+	return http.get<PageResultSysUserPageOutput>('/sysuser/page', { params });
+};
+
+/**
+ * 添加系统用户
+ * @param data 用户信息
+ */
+export const addSysUser = (data: AddSysUserInput) => {
+	return http.post('/sysuser/add', data);
+};
+
+/**
+ * 修改系统用户
+ * @param data 用户信息
+ * @returns
+ */
+export const editSysUser = (data: UpdateSysUserInput) => {
+	return http.put('/sysuser/edit', data);
+};
+
+/**
+ * 删除系统用户
+ * @param id 系统用户id
+ * @returns
+ */
+export const deleteSysUser = (id: number) => {
+	return http.delete('/sysuser/delete', { data: { id } });
+};
