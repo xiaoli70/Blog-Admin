4ff27471fdba1dfa096939e3efc3960450eff08f
念舊 <823302316@qq.com>
2023-07-21 16:49:41 +0800

    增加内容

---
+++ src/frontend/vite-ssr-blog/api/CommentApi.ts
@@ -0,0 +1,52 @@
+import type { PageResultCommentOutput } from "./models/page-result-comment-output";
+import http from "~/utils/http";
+import type { CommentPageQueryInput } from "./models/comment-page-query-input";
+import type { AddCommentInput, PageResultReplyOutput } from "./models";
+
+class CommentApi {
+  /**
+   * 评论或者留言列表
+   * @param params
+   * @returns
+   */
+  list = (params: CommentPageQueryInput) => {
+    return http.get<PageResultCommentOutput>("/comment", { params });
+  };
+
+  /**
+   *
+   * @param params 回复分页查询
+   * @returns
+   */
+  replyList = (params: CommentPageQueryInput) => {
+    return http.get<PageResultReplyOutput>("/comment/replylist", { params });
+  };
+
+  /**
+   *
+   * @param data 评论、回复
+   * @returns
+   */
+  add = (data: AddCommentInput) => {
+    return http.post("/comment", data);
+  };
+
+  /**
+   * 点赞
+   * @param id 对象ID
+   * @returns
+   */
+  praise = (id: number) => {
+    return http.post("/comment/praise", { id: id });
+  };
+  /**
+   * 取消点赞
+   */
+  cancelPraise = (id: number) => {
+    return http.delete("/comment/cancelpraise", {
+      data: { id },
+    });
+  };
+}
+
+export default new CommentApi();
